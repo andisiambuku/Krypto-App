7048e0b0e05899e1304e6a7fddf45d7b99b3ee7a
Roseland Andisi Ambuku <roselandambuku@gmail.com>
2023-09-16 22:57:01 +0300

    adds styling to homepage data

--- components/CoinMarkets.tsx
+++ components/CoinMarkets.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link'
 import Image from 'next/image'
+import alt from '@/assets/alt coin.jpg'
 
 interface Coins {
     id: string,
@@ -8,6 +8,8 @@ interface Coins {
     image: string,
     current_price: number,
     market_cap_rank: number,
+    market_cap: number,
+    total_volume: number,
     price_change_percentage_24h: number,
 
 }
@@ -18,25 +20,41 @@ interface Props {
 
 export default function Coins({ coin }: Props){
     return(
-        <div className='p-10'>
-            {coin.map((coin) =>(
-                 <Link key={coin.id} href={`#`}>
-                    <Image
-                                src={coin.image}
-                                alt={coin.name}
-                                width="50"
-                                height="10"
-                                className="w-full h-full rounded-full"
-                            />
-                    <ul>
-                        <li>{coin.name}{coin.symbol}</li>
-                        <li>Current Price: ${coin.current_price}</li>
-                        <li>Market Cap Rank: {coin.market_cap_rank}</li>
-                        <li>Percentage change of price in 24 hours: {coin.price_change_percentage_24h}</li>
-                    </ul>
-                 </Link>
-            ))}
-           
-        </div>
+        <div className='p-10 flex flex-col overflow-x-auto'>
+            <div className="sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+            <div className="overflow-x-auto"></div>
+                <table className="min-w-full text-left text-sm font-light">
+                    <thead className="border-b font-medium dark:border-neutral-200">
+                        <tr>
+                            <th className="px-6 py-4">Heading</th>
+                            <th className="px-6 py-4">Heading</th>
+                            <th className="px-6 py-4">Heading</th>
+                            <th className="px-6 py-4">Heading</th>
+                            <th className="px-6 py-4">Heading</th>
+                            <th className="px-6 py-4">Heading</th>
+                            <th className="px-6 py-4">Heading</th>
+                            <th className="px-6 py-4">Heading</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {coin.map((coin) =>(
+                            <tr className="border-b dark:border-neutral-200" key={coin.id}>
+                                <td className="whitespace-nowrap px-6 py-4">{coin.id}</td>
+                                <td className="whitespace-nowrap px-6 py-4">{coin.symbol}</td>
+                                <td className="whitespace-nowrap px-6 py-4"><Image src={coin.image || alt} alt={"coin image"} width={30} height={30}/></td>
+                                <td className="whitespace-nowrap px-6 py-4">${coin.current_price}</td>
+                                <td className="whitespace-nowrap px-6 py-4">{coin.market_cap_rank}</td>
+                                <td className="whitespace-nowrap px-6 py-4">{coin.market_cap}</td>
+                                <td className="whitespace-nowrap px-6 py-4">{coin.total_volume}</td>
+                                <td className="whitespace-nowrap px-6 py-4">{coin.price_change_percentage_24h}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+            </div>
+            </div>
+
     )
 }
