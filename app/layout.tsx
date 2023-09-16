ab515b7bf2e6a4644490f17fa4cb5379a58552e6
Roseland Andisi Ambuku <roselandambuku@gmail.com>
2023-09-16 13:12:54 +0300

    setup

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Montserrat } from 'next/font/google'
+
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  weight: '400'
+})
+
+export const metadata: Metadata = {
+  title: 'Krypto App',
+  description: 'A cryptocurrency market analysis app',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={montserrat.className}>{children}</body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <h1>
+      Hello MF World
+    </h1>
+  )
+}
