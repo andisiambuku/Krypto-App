07ced0d91b07a19026d1440644637f633fa5d0f1
Roseland Andisi Ambuku <roselandambuku@gmail.com>
2023-09-25 22:22:24 +0300

    added search container and clearing it

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,6 +2,9 @@
 
 import { CoinSearch, SearchResponse } from '@/types/coinTypes';
 import React, { useState } from 'react';
+import Image from 'next/image'
+import alt from '@/assets/alt coin.jpg'
+import Link from 'next/link';
 
 
 export default function SearchBar() {
@@ -10,6 +13,9 @@ export default function SearchBar() {
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setQuery(e.target.value);
+    if (e.target.value.trim() === '') {
+      setSearchResults(undefined);
+    }
   };
 
   const handleSearchClick = async () => {
@@ -36,13 +42,13 @@ export default function SearchBar() {
       // Handle errors, e.g., display an error message to the user
     }
   };
-  console.log(searchResults)
+  
 
   return (
     <div className="ml-5 flex w-[30%] items-center justify-between relative">
       <input
         type="search"
-        className="relative m-0 block w-[1px] min-w-0 flex-auto  border border-solid border-[#f59e0b] bg-transparent bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6]  outline-none transition duration-200 ease-in-out"
+        className="relative m-0 block w-[1px] min-w-0 flex-auto  border border-solid border-[#f59e0b] bg-transparent bg-clip-padding px-3 py-[0.25rem] font-normal leading-[1.6]  outline-none transition duration-200 ease-in-out"
         placeholder="Search"
         aria-label="Search"
         aria-describedby="button-addon2"
@@ -53,7 +59,7 @@ export default function SearchBar() {
       <button
         type="button"
         aria-label="Search"
-        className="input-group-text flex items-center  px-3 py-2 text-center text-base font-normal text-white bg-[#f59e0b]"
+        className="input-group-text flex items-center  px-3 py-2 text-center font-normal text-white bg-[#f59e0b]"
         id="basic-addon2"
         onClick={handleSearchClick}
       >
@@ -72,14 +78,13 @@ export default function SearchBar() {
       </button>
       {/* Display search results */}
       {searchResults?.coins != null && searchResults?.coins.length> 0 && (
-        <div style={{position:'absolute', top: '120px',height:'250px',overflow:'scroll',background:'white',zIndex:2, width:'300px'}} className="">
+        <div style={{position:'absolute', top: '45px',height:'250px',overflow:'scroll',background:'white',zIndex:2, width:'365px'}} className="">
           <ul>
             {searchResults?.coins.map((result: CoinSearch) => (
-              <li key={result.id}>
-                <div>{result.api_symbol}</div>
-                <div>{result.name}</div>
-                <div>{result.market_cap_rank}</div>
-                <div>{result.thumb}</div> 
+              <li key={result.id} className="flex items-center p-3">
+                <div className="p-1"><Image src={result.thumb || alt} alt={"coin image"} width={30} height={30}/></div>
+                <div className="p-1 text-neutral-500"><Link href={`/coins/${result.id}`}>{result.name}</Link></div>
+                <div className="ml-auto text-neutral-500 text-xs">#{result.market_cap_rank}</div>
                </li>
             ))}
           </ul>
@@ -88,5 +93,4 @@ export default function SearchBar() {
     </div>
   );
 }
-//TODO add image tags to search results, ind a way to unmount the search bar after you clear results
 
