a7dbe0a572bb9c7e47cdf27760fca20f47873cf8
Roseland Andisi Ambuku <roselandambuku@gmail.com>
2024-01-03 11:09:18 +0300

    market chart per id

--- app/coins/[id]/page.tsx
+++ app/coins/[id]/page.tsx
@@ -1,21 +1,27 @@
-'use client'
+"use client";
 import { Suspense } from "react";
 import { CoinPage } from "../../../components/CoinDetails";
 import { useCoinDetails } from "@/hooks/useCoinDetails";
 
-
-export default function Page({params}: { params: {id:string}}){
-    const {data,error,isLoading} = useCoinDetails(params.id)
-    return(
-        <Suspense fallback={<div>Loading coin details...</div>}>
-            {isLoading ? (
-                <div>Loading coin details..</div>
-            ):error ?(
-                <div>Error loading coin details: {error.message}</div>
-            ): (
-                <CoinPage promise={data}/>
-            )} 
-            
-        </Suspense>
-    )
+export default function Page({ params }: { params: { id: string } }) {
+  const { data, error, isLoading } = useCoinDetails(params.id);
+  return (
+    <Suspense
+      fallback={
+        <div className="flex items-center justify-center h-screen">
+          <div className="animate-spin rounded-full border-4 border-solid border-current border-r-transparent h-24 w-24"></div>
+        </div>
+      }
+    >
+      {isLoading ? (
+        <div className="flex items-center justify-center h-screen">
+          <div className="animate-spin rounded-full border-4 border-solid border-current border-r-transparent h-24 w-24"></div>
+        </div>
+      ) : error ? (
+        <div>Error loading coin details: {error.message}</div>
+      ) : (
+        <CoinPage promise={data} />
+      )}
+    </Suspense>
+  );
 }
